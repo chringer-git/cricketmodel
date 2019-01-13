af1335cc3ed771c63cfb4ad65206dde126b3eae1
chringer <chris.ermatinger@gmail.com>
2019-01-13 07:24:29 -0500

    add reference to dartboard radius, remove hard coded value

--- R/throw_dart.R
+++ R/throw_dart.R
@@ -4,11 +4,12 @@
 #' @param target_y y coordinate of the target
 #' @param sd_factor standard deviation
 #' @param random_seed random seed
+#' @param r radius of dartboard (to maximum of outer ring)
 #'
 #' @return coordinates of shot hit
 #'
 #' @export
-throw_dart <- function(target_x, target_y, sd_factor, random_seed) {
+throw_dart <- function(target_x, target_y, sd_factor, random_seed, r) {
 
   # Set random seed if you want to reproduce an exmaple.
   if(!missing(random_seed)){
@@ -19,8 +20,8 @@ throw_dart <- function(target_x, target_y, sd_factor, random_seed) {
   if (missing(sd_factor)) {
     sd_factor <- 1
     }
-  x <- target_x + (8.875/3) * rnorm(1, mean = 0, sd = sd_factor)
-  y <- target_y + (8.875/3) * rnorm(1, mean = 0, sd = sd_factor)
+  x <- target_x + (r/3) * rnorm(1, mean = 0, sd = sd_factor)
+  y <- target_y + (r/3) * rnorm(1, mean = 0, sd = sd_factor)
 
   # Build a list for the placement of the shot.
   coords <- list()
